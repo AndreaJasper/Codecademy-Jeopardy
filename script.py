60876e64ab619ebb71732ee59c6c61c9a950ca5c
jasper_a <andrea.nicole.jasper@gmail.com>
2024-02-21 20:31:37 -0700

    functions for first 3 questions

--- script.py
+++ script.py
@@ -1,5 +1,18 @@
 import pandas as pd
 pd.set_option('display.max_colwidth', -1)
 
-df = pd.read_csv('jeopardy.csv')
-print(df)
+# Loads data
+jeopardy_data = pd.read_csv('jeopardy.csv')
+
+# Renames misformatted columns
+jeopardy_data = jeopardy_data.rename(columns={' Air Date': 'Air Date', ' Round': 'Round', ' Category': 'Category', ' Value': 'Value', ' Question': 'Question', ' Answer': 'Answer'})
+print(jeopardy_data['Question'])
+
+# filter by list of words
+def filter_data(data, words):
+  filter = lambda x: all(word in x for word in words)
+  return data.loc[data['Question'].apply(filter)]
+
+# checks filter
+filtered = filter_data(jeopardy_data, ['King', 'England'])
+print(filtered['Question'])
